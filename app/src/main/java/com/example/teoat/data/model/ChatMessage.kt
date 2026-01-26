07dba23d8e5cffc488fb0be326bb644ad3bd74f0
hyun-1118 <dingding4563@naver.com>
2026-01-27 00:42:50 +0900

    Update ChatMessage.kt

--- app/src/main/java/com/example/teoat/data/model/ChatMessage.kt
+++ app/src/main/java/com/example/teoat/data/model/ChatMessage.kt
@@ -3,7 +3,7 @@ package com.example.teoat.data.model
 data class ChatMessage(
     val id      : String = java.util.UUID.randomUUID().toString(),
     val text    : String,
-    val isUser  : Boolean,          
-    val isPending : Boolean = false,  
-    val action : String?= null     
+    val isUser  : Boolean,          // true=사용자가 보낸 메세지(오른쪽에 표시), false= AI(왼쪽에 표시)
+    val isPending : Boolean = false, // 로딩 중 표시용
+    val action : String?= null      // 이동할 명령어 받는용
 )
