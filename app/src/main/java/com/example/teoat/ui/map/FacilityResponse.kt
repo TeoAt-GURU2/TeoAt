a6fe44cfc89f096c7ec4d3659f6b4f5554e00f27
hyun-1118 <dingding4563@naver.com>
2026-01-27 00:52:56 +0900

    Update FacilityResponse.kt

--- app/src/main/java/com/example/teoat/ui/map/FacilityResponse.kt
+++ app/src/main/java/com/example/teoat/ui/map/FacilityResponse.kt
@@ -9,9 +9,9 @@ data class Youngbgfacltinst(
 )
 
 data class FacilityItem(
-    val INST_NM: String?,            // 기관명 (No.9)
-    val TELNO: String?,              // 전화번호 (No.10)
-    val REFINE_ROADNM_ADDR: String?, // 소재지도로명주소 (No.14)
-    val REFINE_WGS84_LAT: String?,   // WGS84위도 (No.17)
-    val REFINE_WGS84_LOGT: String?   // WGS84경도 (No.18)
+    val INST_NM: String?,            // 기관명  
+    val TELNO: String?,              // 전화번호  
+    val REFINE_ROADNM_ADDR: String?, // 소재지도로명주소  
+    val REFINE_WGS84_LAT: String?,   // WGS84위도  
+    val REFINE_WGS84_LOGT: String?   // WGS84경도  
 )
